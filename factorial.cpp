a4248f5744d707fb67c6059fc4c487cf2f5c7fda
abstrxctive <murisa833@gmail.com>
2025-09-06 22:56:08 +0300

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void factorial(int ParN, int ParK = 1) {
+    for (int i = 1; i <= ParN; i++) {
+        ParK = ParK * i;
+    }
+
+    cout << "Result: " << ParK << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter a factorial-number " << endl;
+    cin >> n;
+
+    factorial(n);
+
+    return 0;
+}
