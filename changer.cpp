82d789fcd05a68a08be1b73ffbe0209d2c133afd
abstrxctive <murisa833@gmail.com>
2025-09-06 22:17:26 +0300

    Create changer.cpp

---
+++ changer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void changer(int ParX, int ParY, int ParTmp) {
+    cout << "before: " << ParX << " ; " << ParY << endl;
+
+    ParTmp = ParX;
+    ParX = ParY;
+    ParY = ParTmp;
+
+    cout << "after: " << ParX << " ; " << ParY << endl;
+
+}
+
+int main() {
+    int x = 1;
+    int y = 2;
+    int tmp;
+
+    changer(x, y, tmp);
+
+    return 0;
+}
