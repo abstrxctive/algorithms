8a4d155f757093a59bf05f7c1e9c1c97ccd42409
abstrxctive <murisa833@gmail.com>
2025-09-07 15:33:32 +0300

    Create triangle_possible.c

---
+++ triangle_possible.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float x, y, z;
+    printf("Enter a first number: ");
+    scanf("%f", &x);
+    printf("Enter a second number: ");
+    scanf("%f", &y);
+    printf("Enter a third number: ");
+    scanf("%f", &z);
+
+    if (x+y < z || x+z < y || y+z < x) {
+        printf("Triangle with sides: ");
+        printf("%.6g %.6g %.6g", x, y, z); 
+        printf(" is impossible");
+    }
+    else {
+        printf("Triangle with sides: ");
+        printf("%.6g %.6g %.6g", x, y, z);
+        printf(" is possible");
+    }
+
+    return 0;
+}
